fabe21872d0cf3c3d963d38bbb241717e4ac6e13
Sunrisepeak <speakshen@163.com>
2021-11-25 02:18:05 +0800

    1.add Affine Transform framworks base 2.fix format of Makefile & README.md
    Signed-off-by: Sunrisepeak <speakshen@163.com>

--- libs/TMATH/test/test_homocoordinate.cpp
+++ libs/TMATH/test/test_homocoordinate.cpp
@@ -3,8 +3,8 @@
 
 #include <homocoordinates.hpp>
 
-template<typename T, unsigned int N>
-std::ostream & operator<<(std::ostream &os, const TMATH::HomoCoordinates<T, N> &hc) {
+template<unsigned int N>
+std::ostream & operator<<(std::ostream &os, const TMATH::HomoCoordinates<N> &hc) {
     os << "(";
     for (int i = 0; i < hc.size() - 1; i++) {
         os << hc[i] << ", ";
@@ -14,12 +14,12 @@ std::ostream & operator<<(std::ostream &os, const TMATH::HomoCoordinates<T, N> &
 
 int main() {
     {
-        TMATH::HomoCoordinates<int, 4> a(1, 2, 3, 1), b(1, 3, 0, 3);
+        TMATH::HomoCoordinates<4> a(1, 2, 3, 1), b(1, 3, 0, 3);
         
         std::cout << a << b << std::endl;
     }
     
-    TMATH::HomoCoordinates<double, 5> a(1, 2, 3, 1, 4), b(1, 3, 0, 3, 0);
+    TMATH::HomoCoordinates<5> a(1, 2, 3, 1, 4), b(1, 3, 0, 3, 0);
 
     std::cout << a << b << std::endl;
 
--- tools/module_name.mk
+++ tools/module_name.mk
@@ -1,14 +1,14 @@
-base			:= test/base
+basef           := test/base
 coordinate_line	:= test/coordinate_line
-straight_line 	:= test/straight_line
+straight_line   := test/straight_line
 circle          := test/circle
-rectangle		:= test/rectangle
+rectangle       := test/rectangle
 
 MODULES_LIST := moduels: \
                 base \
-	            coordinate_line \
-				straight_line \
-				circle \
-				rectangle
+                coordinate_line \
+                straight_line \
+                circle \
+                rectangle
 
 MODULES_INFO := $(addsuffix \n,$(MODULES_LIST))
