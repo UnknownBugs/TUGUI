84c997f02dab9a740f43b332f334f2c66d58116d
invoker_qq <478880214@qq.com>
2021-11-26 12:56:38 +0800

    add triangle demo
    Signed-off-by: invoker_qq <478880214@qq.com>

--- core/tugui.hpp
+++ core/tugui.hpp
@@ -61,10 +61,14 @@ public:
         if (y1 == y2) drawXLine(y1, x1, x2, rgb);
         __mLinearEquation.set(x1, y1, x2, y2);
         double x = x1;
-        double deltaX { 1 };
+        double deltaX{1};
         if (__mLinearEquation.getSlope() > 1) // deltaY = 1
             deltaX = 1. / __mLinearEquation.getSlope();
-        while (x < x2) {
+        double tx = x2;
+        if (x > tx)
+            swap(x, tx);
+        while (x < tx)
+        {
             drawPixel(x, __mLinearEquation.getY(x), rgb);
             x += deltaX;
         }
@@ -91,6 +95,9 @@ public:
 
     void drawRectangle(Rectangle rect, RGB rgb = PIXEL_WHITE);
 
+    void drawTriangle(unsigned int x1, unsigned int y1, unsigned int x2, unsigned int y2, unsigned int x3, unsigned int y3, RGB rgb = PIXEL_WHITE);
+
+    void fillTriangle( int x1,  int y1,  int x2,  int y2,  int x3,  int y3, RGB rgb = PIXEL_WHITE);
     /**
      *  F(x, y) = x^2 + y^2 -R^2 
      *  1/8 circle of first quadrant(above)
@@ -147,6 +154,247 @@ void Graphics::drawRectangle(unsigned int x, unsigned int y, unsigned int w, uns
 
 void Graphics::drawRectangle(Rectangle rect, RGB rgb) {
     drawRectangle(rect.x, rect.y, rect.w, rect.h, rgb);
+}
+
+void Graphics::drawTriangle(unsigned int x1, unsigned int y1, unsigned int x2, unsigned int y2, unsigned int x3, unsigned int y3, RGB rgb)
+{
+    drawLine(x1, y1, x2, y2, rgb);
+    drawLine(x2, y2, x3, y3, rgb);
+    drawLine(x3, y3, x1, y1, rgb);
+}
+
+void Graphics::fillTriangle(int x1, int y1,  int x2,  int y2,  int x3,  int y3, RGB rgb)
+{
+    int t1x, t2x, y, minx, maxx, t1xp, t2xp;
+    bool changed1 = false;
+    bool changed2 = false;
+    int signx1, signx2, dx1, dy1, dx2, dy2;
+    int e1, e2;
+    // Sort vertices
+    if (y1 > y2)
+    {
+        swap(y1, y2);
+        swap(x1, x2);
+    }
+    if (y1 > y3)
+    {
+        swap(y1, y3);
+        swap(x1, x3);
+    }
+    if (y2 > y3)
+    {
+        swap(y2, y3);
+        swap(x2, x3);
+    }
+
+    t1x = t2x = x1;
+    y = y1; // Starting points
+    dx1 =(x2 - x1);
+    if (dx1 < 0)
+    {
+        dx1 = -dx1;
+        signx1 = -1;
+    }
+    else
+        signx1 = 1;
+    dy1 = (y2 - y1);
+
+    dx2 = (x3 - x1);
+    if (dx2 < 0)
+    {
+        dx2 = -dx2;
+        signx2 = -1;
+    }
+    else
+        signx2 = 1;
+    dy2 = (y3 - y1);
+
+    if (dy1 > dx1)
+    { // swap values
+        swap(dx1, dy1);
+        changed1 = true;
+    }
+    if (dy2 > dx2)
+    { // swap values
+        swap(dy2, dx2);
+        changed2 = true;
+    }
+
+    e2 = (dx2 >> 1);
+    // Flat top, just process the second half
+    if (y1 == y2)
+        goto next;
+    e1 = (dx1 >> 1);
+
+    for (int i = 0; i < dx1;)
+    {
+        t1xp = 0;
+        t2xp = 0;
+        if (t1x < t2x)
+        {
+            minx = t1x;
+            maxx = t2x;
+        }
+        else
+        {
+            minx = t2x;
+            maxx = t1x;
+        }
+        // process first line until y value is about to change
+        while (i < dx1)
+        {
+            i++;
+            e1 += dy1;
+            while (e1 >= dx1)
+            {
+                e1 -= dx1;
+                if (changed1)
+                    t1xp = signx1; //t1x += signx1;
+                else
+                    goto next1;
+            }
+            if (changed1)
+                break;
+            else
+                t1x += signx1;
+        }
+        // Move line
+    next1:
+        // process second line until y value is about to change
+        while (1)
+        {
+            e2 += dy2;
+            while (e2 >= dx2)
+            {
+                e2 -= dx2;
+                if (changed2)
+                    t2xp = signx2; //t2x += signx2;
+                else
+                    goto next2;
+            }
+            if (changed2)
+                break;
+            else
+                t2x += signx2;
+        }
+    next2:
+        if (minx > t1x)
+            minx = t1x;
+        if (minx > t2x)
+            minx = t2x;
+        if (maxx < t1x)
+            maxx = t1x;
+        if (maxx < t2x)
+            maxx = t2x;
+        drawXLine(y, minx, maxx, rgb); // Draw line from min to max points found on the y
+                                       // Now increase y
+        if (!changed1)
+            t1x += signx1;
+        t1x += t1xp;
+        if (!changed2)
+            t2x += signx2;
+        t2x += t2xp;
+        y += 1;
+        if (y == y2)
+            break;
+    }
+next:
+    // Second half
+    dx1 = (x3 - x2);
+    if (dx1 < 0)
+    {
+        dx1 = -dx1;
+        signx1 = -1;
+    }
+    else
+        signx1 = 1;
+    dy1 = (y3 - y2);
+    t1x = x2;
+
+    if (dy1 > dx1)
+    { // swap values
+        swap(dy1, dx1);
+        changed1 = true;
+    }
+    else
+        changed1 = false;
+
+    e1 = (dx1 >> 1);
+
+    for (int i = 0; i <= dx1; i++)
+    {
+        t1xp = 0;
+        t2xp = 0;
+        if (t1x < t2x)
+        {
+            minx = t1x;
+            maxx = t2x;
+        }
+        else
+        {
+            minx = t2x;
+            maxx = t1x;
+        }
+        // process first line until y value is about to change
+        while (i < dx1)
+        {
+            e1 += dy1;
+            while (e1 >= dx1)
+            {
+                e1 -= dx1;
+                if (changed1)
+                {
+                    t1xp = signx1;
+                    break;
+                } //t1x += signx1;
+                else
+                    goto next3;
+            }
+            if (changed1)
+                break;
+            else
+                t1x += signx1;
+            if (i < dx1)
+                i++;
+        }
+    next3:
+        // process second line until y value is about to change
+        while (t2x != x3)
+        {
+            e2 += dy2;
+            while (e2 >= dx2)
+            {
+                e2 -= dx2;
+                if (changed2)
+                    t2xp = signx2;
+                else
+                    goto next4;
+            }
+            if (changed2)
+                break;
+            else
+                t2x += signx2;
+        }
+    next4:
+        if (minx > t1x)
+            minx = t1x;
+        if (minx > t2x)
+            minx = t2x;
+        if (maxx < t1x)
+            maxx = t1x;
+        if (maxx < t2x)
+            maxx = t2x;
+        drawXLine(y,minx, maxx,rgb);
+        if (!changed1)
+            t1x += signx1;
+        t1x += t1xp;
+        if (!changed2)
+            t2x += signx2;
+        t2x += t2xp;
+        y += 1;
+        if (y > y3)
+            return;
+    }
 }
 
 void Graphics::drawCircle(unsigned int x0, unsigned int y0, unsigned int r, RGB rgb) {
