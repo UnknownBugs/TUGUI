3b3a542c6672addac2549e07832653586bde9beb
invoker__qq <478880214@qq.com>
2021-11-29 14:41:09 +0800

    Update uefi_base_interface_impl.hpp

--- platform/uefi/uefi_base_interface_impl.hpp
+++ platform/uefi/uefi_base_interface_impl.hpp
@@ -6,67 +6,66 @@
 #include <uefi/wrapper/gop_wrapper.hpp>
 #include <uefi/wrapper/systemtable_wrapper.hpp>
 
-
-
 class BaseInterfaceImpl : public TUGUI::INTERFACE::BaseInterface {
-  using ST = UEFIWrapper::SystemTable;
-  using EST = UEFIWrapper::SystemTable::ESystemTable;
+    using ST = UEFIWrapper::SystemTable;
+    using EST = UEFIWrapper::SystemTable::ESystemTable;
 
- public:
-  void init(EST *est) {
-    ST::init(est);
-    ST::setWatchdogTimer(0, 0, 0, nullptr);
-    ST::clearScreen();
-    __mGOP = UEFIWrapper::GOP();  // init gop ptr
-  }
+   public:
+    void init(EST *est) {
+        ST::init(est);
+        ST::setWatchdogTimer(0, 0, 0, nullptr);
+        ST::clearScreen();
+        __mGOP = UEFIWrapper::GOP();  // init gop ptr
+    }
 
-  uint32_t getFrameBufferBase() const override {
-    return __mGOP.getFrameBufferBase();
-  }
+    uint32_t getFrameBufferBase() const override {
+        return __mGOP.getFrameBufferBase();
+    }
 
-  uint32_t getHorizontalResolution() const override {
-    return __mGOP.getHorizontalResolution();
-  }
+    uint32_t getHorizontalResolution() const override {
+        return __mGOP.getHorizontalResolution();
+    }
 
-  uint32_t getVerticalResolution() const override {
-    return __mGOP.getVerticalResolution();
-  }
+    uint32_t getVerticalResolution() const override {
+        return __mGOP.getVerticalResolution();
+    }
 
-  void drawPixel(uint32_t x, uint32_t y, TUGUI::RGB rgb) override {
-    uint32_t hr = getHorizontalResolution();
-    UEFIWrapper::GOP::Pixel *base =
-        (UEFIWrapper::GOP::Pixel *)(__mGOP.getFrameBufferBase());
-    UEFIWrapper::GOP::Pixel *pixel = base + (hr * y) + x;
+    void drawPixel(uint32_t x, uint32_t y, TUGUI::RGB rgb) override {
+        uint32_t hr = getHorizontalResolution();
+        UEFIWrapper::GOP::Pixel *base =
+            (UEFIWrapper::GOP::Pixel *)(__mGOP.getFrameBufferBase());
+        UEFIWrapper::GOP::Pixel *pixel = base + (hr * y) + x;
 
-    pixel->red = rgb.R;
-    pixel->green = rgb.G;
-    pixel->blue = rgb.B;
-    pixel->reserved = rgb.S;
-  }
+        pixel->red = rgb.R;
+        pixel->green = rgb.G;
+        pixel->blue = rgb.B;
+        pixel->reserved = rgb.S;
+    }
 
-  TDEBUG::RT clearScrean() override {
-    UEFIWrapper::SystemTable::clearScreen();
-    return TDEBUG::RT::SUCCESS;
-  }
+    TDEBUG::RT clearScrean() override {
+        UEFIWrapper::SystemTable::clearScreen();
+        return TDEBUG::RT::SUCCESS;
+    }
 
-  void *tuguiMalloc(uint64_t size) override {
-    return UEFIWrapper::SystemTable::allocatePool(EfiConventionalMemory, size);
-  }
+    void *tuguiMalloc(uint64_t size) override {
+        return UEFIWrapper::SystemTable::allocatePool(EfiConventionalMemory,
+                                                      size);
+    }
 
-  void tuguiOutputString(uint16_t *s) override {
-    UEFIWrapper::SystemTable::OutputString(s);
-  }
+    void tuguiOutputString(uint16_t *s) override {
+        UEFIWrapper::SystemTable::OutputString(s);
+    }
 
- private:
-  static UEFIWrapper::GOP __mGOP;
+   private:
+    static UEFIWrapper::GOP __mGOP;
 };  // BaseInterfaceImpl
 
 void *operator new(uint64_t size) {
-  return UEFIWrapper::SystemTable::allocatePool(EfiConventionalMemory, size);
+    return UEFIWrapper::SystemTable::allocatePool(EfiConventionalMemory, size);
 }
 
 void *operator new[](uint64_t size) {
-  return UEFIWrapper::SystemTable::allocatePool(EfiConventionalMemory, size);
+    return UEFIWrapper::SystemTable::allocatePool(EfiConventionalMemory, size);
 }
 
 void operator delete(void *ptr) { UEFIWrapper::SystemTable::freePool(ptr); }
@@ -74,13 +73,13 @@ void operator delete(void *ptr) { UEFIWrapper::SystemTable::freePool(ptr); }
 void operator delete[](void *ptr) { UEFIWrapper::SystemTable::freePool(ptr); }
 
 void operator delete(void *ptr, uint64_t size) {
-  size -= size;  // remove warning: unused parameter
-  UEFIWrapper::SystemTable::freePool(ptr);
+    size -= size;  // remove warning: unused parameter
+    UEFIWrapper::SystemTable::freePool(ptr);
 }
 
 void operator delete[](void *ptr, uint64_t size) {
-  size -= size;  // remove warning: unused parameter
-  UEFIWrapper::SystemTable::freePool(ptr);
+    size -= size;  // remove warning: unused parameter
+    UEFIWrapper::SystemTable::freePool(ptr);
 }
 
 #endif  // __UEFI_BASE_INTERFACE_HPP__
