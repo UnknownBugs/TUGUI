e16b54b34e5fc75dcdf1043823e3cd581ac293ab
sunrisepeak <speakshen@163.com>
2021-12-17 16:07:05 +0800

    1. move UEFIWrapper from libs/ to platfrom/
    Signed-off-by: sunrisepeak <speakshen@163.com>

--- libs/UEFIWrapper/UefiWrapperInclude.mk
+++
@@ -1,2 +0,0 @@
-UEFIWRAPPER_LIB_INC := include/Uefi \
-                       core/
--- libs/UEFIWrapper/core/UEFIWrapper.hpp
+++
@@ -1,8 +0,0 @@
-#ifndef __UEFIWrapper__HPP__
-#define __UEFIWrapper__HPP__
-
-#include <wrapper/event_wrapper.hpp>
-#include <wrapper/gop_wrapper.hpp>
-#include <wrapper/event_wrapper.hpp>
-
-#endif // __UEFIWrapper__HPP__
