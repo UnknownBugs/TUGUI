3391897f90456d8ad0caddb3df0fd441903f477b
SPeak <speakshen@163.com>
2021-12-22 12:30:55 +0800

    Revert "modify test case about Line (#50)"
    This reverts commit 6e707291bf40aae96882fd5f21b642ddd505cb4a.

--- test/transformation/line.cpp
+++ test/transformation/line.cpp
@@ -10,11 +10,7 @@ int tuguiMain() {
 
     te.setScale(2);
 
-<<<<<<< HEAD
-    Line l1(50, 50, 100, 100), l2(100, 50, 150, 100), l3(400, 401, 410, 411);
-=======
     Line l1(50, 50, 100, 100), l2(100, 50, 150, 100), l3(400, 401, 501, 501);
->>>>>>> ea705373327891cd191e99855c33a6fa4f9f454f
 
     pe.setColor(Color(BLUE, GREEN));
     pe.setColorGradient(50);
@@ -28,17 +24,10 @@ int tuguiMain() {
 
     te.setPoint(400, 401);
     te.setRotation(35);
-<<<<<<< HEAD
-    pe.setColor(Color(YELLOW, VIOLET));
-    pe.setColorGradient(150);
-
-    uint8_t rTimes = 10;
-=======
     pe.setColor(Color(RED, VIOLET));
     pe.setColorGradient(50);
 
     uint8_t rTimes = 50;
->>>>>>> ea705373327891cd191e99855c33a6fa4f9f454f
 
     for (uint8_t i = 0; i < rTimes; i++) {
         pe(l3);
