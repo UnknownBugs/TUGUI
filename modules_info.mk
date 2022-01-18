80babfb69da5abceb0f1b44e38d061c199e80cd4
sunrisepeak <speakshen@163.com>
2022-01-18 23:03:56 +0800

    1. adjust test program file name 2. complete batch compiler function for makefile
    Signed-off-by: sunrisepeak <speakshen@163.com>

--- modules_info.mk
+++ modules_info.mk
@@ -1,8 +1,11 @@
 debug	            := test/debug
+gradient            := test/gradient
 
 ALL_MODULES := $(debug) \
+               $(gradient)
 
 MODULES_LIST := moduels: \
                 debug \
+                gradient
 
 MODULES_INFO := $(addsuffix \n,$(MODULES_LIST))
