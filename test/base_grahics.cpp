9839632ad359a8e236e8ac29e546877ba8b5789b
Sunrisepeak <speakshen@163.com>
2022-01-24 02:32:28 +0800

    add exit func for test program

--- test/base_grahics.cpp
+++ test/base_grahics.cpp
@@ -1,3 +1,5 @@
+#include <iostream>
+
 #include <tugui.hpp>
 //#include <initializer_list>
 
@@ -14,13 +16,17 @@ int TUGUI_MAIN() {
     Rectangle rect4(760, 500, 100, 100), rect5(370, 390, 100, 300), rect6(630, 390, 100, 150);
 
     pe.setColor(RED); pe(rect1);
+    std::cin.get();
     pe.setColor(ORANGE); pe(rect2);
+    std::cin.get();
     pe.setColor(YELLOW); pe(rect3);
+    std::cin.get();
     pe.setColor(GREEN); pe(rect4);
+    std::cin.get();
     pe.setColor(CYAN); pe(rect5);
+    std::cin.get();
     pe.setColor(BLUE); pe(rect6);
-
-    while(1);
+    std::cin.get();
     
     return 0;
 }
