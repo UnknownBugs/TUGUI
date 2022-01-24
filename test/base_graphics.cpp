1daece0c753aa255eca88c16ab87996aefbe8523
SPeak <speakshen@163.com>
2022-01-24 22:36:24 +0800

    adjust graphics test demo (#67)

--- test/base_graphics.cpp
+++ test/base_graphics.cpp
@@ -11,28 +11,30 @@ PaintEngine pe;
 
 
 inline void line() {
-    Line l1(5, 5, 5, 100), l2(5, 5, 100, 5);
+    Line l1(5, 5, 5, 200), l2(5, 5, 200, 5);
 
     pe.setColor(VIOLET); pe(l1);
     pe.setColor(BLUE); pe(l2);
 
-    pe.setColor(WHITE); pe(Line(5, 5, 100, 100));
-    pe.setColor(GREEN); pe(Line(66, 88, 5, 5));
-    pe.setColor(CYAN); pe(Line(88, 66, 5, 5));
+    pe.setColor(WHITE); pe(Line(5, 5, 200, 200));
+    pe.setColor(GREEN); pe(Line(166, 188, 5, 5));
+    pe.setColor(CYAN); pe(Line(188, 166, 5, 5));
 }
 
 inline void rectangle() {
-    Rectangle rect1(100, 100, 50, 50), rect2(100, 100, 100, 70);
+    Rectangle rect1(200, 200, 150, 150), rect2(200, 200, 200, 170);
     pe.setColor(RED); pe(rect1);
     pe.setColor(GREEN); pe(rect2);
 }
 
 inline void circle() {
-
+    Circle c(650, 370, 250);
+    pe.setColor(VIOLET); pe(c);
 }
 
 inline void triangle() {
-
+    Triangle t(650, 370, 1400, 700, 800, 900);
+    pe.setColor(ORANGE); pe(t);
 }
 
 };
@@ -45,6 +47,12 @@ int TUGUI_MAIN() {
     std::cin.get();
     
     GRAPHICS_TEST::rectangle();
+    std::cin.get();
+    
+    GRAPHICS_TEST::circle();
+    std::cin.get();
+
+    GRAPHICS_TEST::triangle();
     std::cin.get();
     
     //std::cout << "test over" << std::endl;
