a151f1b5afc67bbcc261ed04aa84672033b06e40
Sunrisepeak <speakshen@163.com>
2022-01-24 13:03:20 +0800

    1. adjust test program 2. opt head file for linux platform impl
    Signed-off-by: Sunrisepeak <speakshen@163.com>

--- test/base_grahics.cpp
+++ test/base_grahics.cpp
@@ -5,28 +5,49 @@
 
 using namespace TUGUI;
 
+namespace GRAPHICS_TEST {
+
+PaintEngine pe;
+
+
+inline void line() {
+    Line l1(5, 5, 5, 100), l2(5, 5, 100, 5);
+
+    pe.setColor(VIOLET); pe(l1);
+    pe.setColor(BLUE); pe(l2);
+
+    pe.setColor(WHITE); pe(Line(5, 5, 100, 100));
+    pe.setColor(GREEN); pe(Line(66, 88, 5, 5));
+    pe.setColor(CYAN); pe(Line(88, 66, 5, 5));
+}
+
+inline void rectangle() {
+    Rectangle rect1(100, 100, 50, 50), rect2(100, 100, 100, 70);
+    pe.setColor(RED); pe(rect1);
+    pe.setColor(GREEN); pe(rect2);
+}
+
+inline void circle() {
+
+}
+
+inline void triangle() {
+
+}
+
+};
+
 int TUGUI_MAIN() {
     
     TUGUI::PLATFORM::init(nullptr);
     
-    PaintEngine pe;
+    GRAPHICS_TEST::line();
+    std::cin.get();
     
-    // test Rect
-    Rectangle rect1(50, 50, 300, 400), rect2(500, 500, 100, 30), rect3(240, 500, 100, 80);
-    Rectangle rect4(760, 500, 100, 100), rect5(370, 390, 100, 300), rect6(630, 390, 100, 150);
-
-    pe.setColor(RED); pe(rect1);
+    GRAPHICS_TEST::rectangle();
     std::cin.get();
-    pe.setColor(ORANGE); pe(rect2);
-    std::cin.get();
-    pe.setColor(YELLOW); pe(rect3);
-    std::cin.get();
-    pe.setColor(GREEN); pe(rect4);
-    std::cin.get();
-    pe.setColor(CYAN); pe(rect5);
-    std::cin.get();
-    pe.setColor(BLUE); pe(rect6);
-    std::cin.get();
+    
+    //std::cout << "test over" << std::endl;
     
     return 0;
 }
